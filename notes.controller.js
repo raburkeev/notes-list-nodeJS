320c15777f39369ce00e0e035de91ef50f06d5f5
Roman Burkeev <fneka17@gmail.com>
2022-11-21 21:14:48 +0300

    add remove note by id function

--- notes.controller.js
+++ notes.controller.js
@@ -17,6 +17,14 @@ async function addNote(title) {
     console.log(chalk.green('Note was added!'))
 }
 
+async function removeNote(noteId) {
+    const notes = await getNotes()
+    const updatedNotes = notes.filter(note => note.id !== noteId)
+
+    await fs.writeFile(notesPath, JSON.stringify(updatedNotes))
+    console.log(chalk.red('Note was removed!'))
+}
+
 async function getNotes() {
     const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
 
@@ -28,10 +36,10 @@ async function printNotes() {
 
     console.log(chalk.yellow('Here is the list of notes:'))
     notes.forEach(note => {
-        console.log(chalk.blue(note.title))
+        console.log(chalk.blue(`${note.id} ${note.title}`))
     })
 }
 
 module.exports = {
-    addNote, printNotes
+    addNote, printNotes, removeNote
 }
