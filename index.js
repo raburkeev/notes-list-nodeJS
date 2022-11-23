4266905bf30799939609d62df969fb6995188341
Roman Burkeev <fneka17@gmail.com>
2022-11-23 21:22:07 +0300

    start edit-note-2

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get('/', async (req, res) => {
     })
 })
 
+// app.get(`/:id/:data`, async (req, res) => {
+//
+// })
+
 app.post('/', async (req, res) => {
     await addNote(req.body.title)
     res.render('index', {
--- public/app.js
+++ public/app.js
@@ -11,17 +11,72 @@ document.addEventListener('click', (event) => {
 document.addEventListener('click', (event) => {
     if(event.target.dataset.type === 'edit') {
         const id = event.target.dataset.id
-        const data = prompt('Print new title')
+        console.log(event.target.closest('li'))
 
-        if (data) {
-            edit(id, data).then(() => {
-                event.target.closest('li').innerHTML = `${data}
+        event.target.closest('li').innerHTML = (
+            `<input type="text" id="edit-input" name="newTitle" required/>
+                <div>
+                    <button class="btn btn-primary" data-type="save" data-id="${id}">Save</button>
+                    <button class="btn btn-danger" data-type="cancel" data-id="${id}">Cancel</button>
+                </div>
+            `
+        )
+
+        // const editInput = document.querySelector('#edit-input')
+        // console.log(editInput)
+        // edit(id, editInput.value).then(() => {
+        //     event.target.closest('li').innerHTML = (
+        //         `${editInput.value}
+        //             <div>
+        //                 <button class="btn btn-primary" data-type="edit" data-id="${id}">Edit</button>
+        //                 <button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
+        //             </div>`
+        //     )
+        // }).then(() => {
+        //     event.target.closest('li').innerHTML = initHTML
+        // })
+
+
+        // const data = prompt('Print new title')
+        //
+        // if (data) {
+        //     edit(id, data).then(() => {
+        //         event.target.closest('li').innerHTML = (
+        //             `${data}
+        //             <div>
+        //                 <button class="btn btn-primary" data-type="edit" data-id="${id}">Edit</button>
+        //                 <button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
+        //             </div>`
+        //         )
+        //     })
+        // }
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const id = event.target.dataset.id
+    if(event.target.dataset.type === 'save') {
+        const editInput = document.querySelector('#edit-input')
+        edit(id, editInput.value).then(() => {
+
+        })
+    }
+})
+
+document.addEventListener('click', async (event) => {
+    if(event.target.dataset.type === 'cancel') {
+        const id = event.target.dataset.id
+        const data = await fetch('/', {
+            method: 'GET'
+        })
+        console.log(data)
+        event.target.closest('li').innerHTML = (
+            `
                     <div>
                         <button class="btn btn-primary" data-type="edit" data-id="${id}">Edit</button>
                         <button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
                     </div>`
-            })
-        }
+        )
     }
 })
 
