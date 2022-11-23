4b57058717b95029ac489afd07866bf4504beeb4
Roman Burkeev <fneka17@gmail.com>
2022-11-23 23:30:01 +0300

    data-title

--- public/app.js
+++ public/app.js
@@ -11,12 +11,14 @@ document.addEventListener('click', (event) => {
 document.addEventListener('click', (event) => {
     if(event.target.dataset.type === 'edit') {
         const id = event.target.dataset.id
-        console.log(event.target.closest('li'))
+        console.log(event.target.dataset)
+        // console.log(event.target.value)
+        const noteTitle = document.querySelector(`[data-title="${id}"]`).innerText
 
         event.target.closest('li').innerHTML = (
-            `<input type="text" id="edit-input" name="newTitle" required/>
+            `<input type="text" id="edit-input" name="newTitle" value="${noteTitle}" required/>
                 <div>
-                    <button class="btn btn-primary" data-type="save" data-id="${id}">Save</button>
+                    <button class="btn btn-success" data-type="save" data-id="${id}">Save</button>
                     <button class="btn btn-danger" data-type="cancel" data-id="${id}">Cancel</button>
                 </div>
             `
@@ -59,19 +61,25 @@ document.addEventListener('click', (event) => {
         const editInput = document.querySelector('#edit-input')
         edit(id, editInput.value).then(() => {
 
+            event.target.closest('li').innerHTML = (
+                `<span>${editInput.value}</span> 
+                <div>
+                    <button class="btn btn-primary" data-type="edit" data-id="${id}">Edit</button>
+                    <button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
+                </div>
+  `
+            )
         })
     }
 })
 
-document.addEventListener('click', async (event) => {
+document.addEventListener('click', (event) => {
     if(event.target.dataset.type === 'cancel') {
         const id = event.target.dataset.id
-        const data = await fetch('/', {
-            method: 'GET'
-        })
-        console.log(data)
+        const noteTitle = document.querySelector(`[data-title="${id}"]`).innerText
         event.target.closest('li').innerHTML = (
             `
+                    <span>${noteTitle}</span>
                     <div>
                         <button class="btn btn-primary" data-type="edit" data-id="${id}">Edit</button>
                         <button class="btn btn-danger" data-type="remove" data-id="${id}">&times;</button>
